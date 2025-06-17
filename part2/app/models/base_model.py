c144e12b655818276fe61f560a396475d4719ba8
root <gbrielbbarn@gmail.com>
2025-06-16 21:32:57 -0300

    feat: place and base_model getter/setters, in-memory persistance with repository.py

--- part2/app/models/base_model.py
+++ part2/app/models/base_model.py
@@ -1,21 +1,34 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+import datetime
+import uuid
+
 
 class BaseModel(ABC):
+    def __init__(self):
+        self.__id = str(uuid.uuid4())
+        self.__creation_date = datetime.today()
+        self.__update_date = datetime.today()
+
     @property
-    @abstractmethod
+    def id(self):
+        return self.__id
+
+    @property
     def creation_date(self) -> datetime:
-        pass
+        return self.__creation_date
+
     @property
-    @abstractmethod
     def update_date(self) -> datetime:
-        pass
+        return self.__update_date
 
     @abstractmethod
     def create(self):
         pass
+
     @abstractmethod
     def update(self):
         pass
+
     @abstractmethod
     def delete(self):
         pass
--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -1,14 +1,81 @@
 from base_model import BaseModel
 
+
 class Place(BaseModel):
-    def __init__(self, id_place: str, title: str="n/a",
-                description: str="n/a",
-                price: float=0.00,
-                longitude: tuple=(0, 0),
-                amenities: list=None):
-        self.__id_place = id_place
+    def __init__(
+        self,
+        id_owner: str,
+        title: str = "n/a",
+        description: str = "n/a",
+        price: float = 0.00,
+        coordinates: tuple = (0, 0),
+        amenities: list = None,
+    ):
+        self.__id_owner = id_owner
         self.__title = title
         self.__description = description
         self.__price = price
-        self.__longitude = longitude
+        self.__coordinates = coordinates
         self.__amenities = amenities if amenities is not None else []
+
+    # new_place.update(option, value)
+    #     if hasattr(self, option):
+    #         setattr(self, option, value)
+    #     if (option == "description")
+    #         new_place.description == value
+    #     elif option == "title":
+    #         self.title = value
+
+    # def set_id(self, value):
+    #     self.__id_place = value
+
+    @property
+    def id_owner(self):
+        return self.__id_owner
+
+    # Title
+    @property
+    def title(self):
+        return self.__title
+
+    @title.setter
+    def title(self, value: str):
+        if not isinstance(value, str) or None:
+            raise TypeError
+        self.__title = value
+
+    # Description
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, value):
+        self.__description = value
+
+    # Price
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        self.__price = value
+
+    # Coordinates
+    @property
+    def coordinates(self):
+        return self.__coordinates
+
+    @coordinates.setter
+    def coordinates(self, value):
+        self.__coordinates = value
+
+    # Amenities
+    @property
+    def amenities(self):
+        return self.__amenities
+
+    @amenities.setter
+    def amenities(self, value):
+        self.__amenities = value
