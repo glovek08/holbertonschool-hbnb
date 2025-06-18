821da822a5974441a9385aeeddd337df48841d4b
federico-paganini <perfedefer@gmail.com>
2025-06-18 18:45:33 -0300

    feat: Getters and setters for instance attributes

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -1,7 +1,32 @@
 from base_model import BaseModel
 
+
 class Review(BaseModel):
-    def __init__(self, id_review: str, rating: int=0, comment: str="n/a"):
-        self.__id_review = id_review
-        self.__rating = rating
-        self.__comment = comment
+    def __init__(self, rating: float, comment: str):
+        super().__init__()
+        self.rating = rating
+        self.comment = comment
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value):
+        if not isinstance(value, float):
+            raise TypeError
+
+        if not (0 <= value <= 5):
+            raise ValueError("Incorrect Rating value")
+
+        self.__rating = value
+
+    @property
+    def comment(self):
+        return self.__comment
+
+    @comment.setter
+    def comment(self, value):
+        self.__comment = self.validate_string(value, "comment")
+
+    # self.__name = self.validate_string(value, "name").isalpha()
