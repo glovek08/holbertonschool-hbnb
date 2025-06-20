1c3073de05666d8b63c94b978dc7625a20ce7545
federico-paganini <perdefer@gmail.com>
2025-06-20 18:04:59 -0300

    feat: User namespace register

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_restx import Api
 
+from app.api.v1.users import api as users_ns
+from app.api.v1.places import api as places_ns
+
 
 def create_app():
     app = Flask(__name__)
@@ -11,8 +14,8 @@ def create_app():
         description="HBnB Application API",
         doc="/api/v1/",
     )
-    from app.api.v1.places import api as places_ns
 
+    api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(places_ns, path="/api/v1/places")
     # Placeholder for API namespaces (endpoints will be added later)
     # Additional namespaces for places, reviews, and amenities will be added later
