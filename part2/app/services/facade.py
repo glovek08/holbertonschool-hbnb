843f57c0d4a8460cc758cdddeccbd1c027e86466
glovek08 <gbrielbbarn@gmail.com>
2025-06-20 11:09:44 -0300

    feat: create place method, lacks owner and amenities validation

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,5 @@
-from app.persistence.repository import InMemoryRepository
+from app.persistence.respository import InMemoryRepository
+from models.place import Place
 
 
 class HBnBFacade:
@@ -13,7 +14,41 @@ class HBnBFacade:
         # Logic will be implemented in later tasks
         pass
 
-    # Placeholder method for fetching a place by ID
+    def create_place(self, place_data):
+        required_fields = [
+            "owner",
+            "title",
+            "description",
+            "price",
+            "latitude",
+            "longitude",
+            "amenities",
+        ]
+        for field in required_fields:
+            if field not in place_data:
+                raise ValueError(f"Missing required field: {field}")
+        # No se como manejar el owner. Que llamo? Que le pongo?
+        # Como controlar que el owner exista? fijarse si la id existe en la persistence?
+        # Lo mismo para amenities.
+
+        return Place(
+            owner=place_data["owner"],
+            title=place_data["title"],
+            description=place_data["description"],
+            price=place_data["price"],
+            latitude=place_data["latitude"],
+            longitude=place_data["longitude"],
+            amenities=place_data["amenities"],
+        )
+
     def get_place(self, place_id):
-        # Logic will be implemented in later tasks
+        # Placeholder for logic to retrieve a place by ID, including associated owner and amenities
+        pass
+
+    def get_all_places(self):
+        # Placeholder for logic to retrieve all places
+        pass
+
+    def update_place(self, place_id, place_data):
+        # Placeholder for logic to update a place
         pass
