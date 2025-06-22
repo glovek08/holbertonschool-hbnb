72e2f9939db4917d88a8d098e615968c2f8c4681
federico-paganini <perdefer@gmail.com>
2025-06-22 12:01:56 -0300

    fix: Typo in import

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,4 @@
-from app.persistence.respository import InMemoryRepository
+from app.persistence.repository import InMemoryRepository
 from app.models.place import Place
 from app.models.user import User
 from app.models.amenity import Amenity
