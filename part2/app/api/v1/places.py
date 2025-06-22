8e86f136cb036b81790c375d789d9346b705986f
federico-paganini <perfedefer@gmail.com>
2025-06-22 18:46:12 -0300

    chore: Amenity and User models deletion. Both retrieving when used form db (Verifications only when created or updated self)

--- part2/app/api/v1/places.py
+++ part2/app/api/v1/places.py
@@ -3,26 +3,6 @@ from app.services import facade
 
 api = Namespace("places", description="Place operations")
 
-# Define the models for related entities
-amenity_model = api.model(
-    "PlaceAmenity",
-    {
-        "id": fields.String(description="Amenity ID"),
-        "name": fields.String(description="Name of the amenity"),
-        "description": fields.String(description="Short description of the amenity"),
-    },
-)
-
-user_model = api.model(
-    "PlaceUser",
-    {
-        "id": fields.String(description="User ID"),
-        "first_name": fields.String(description="First name of the owner"),
-        "last_name": fields.String(description="Last name of the owner"),
-        "email": fields.String(description="Email of the owner"),
-    },
-)
-
 # Define the place model for input validation and documentation
 place_model = api.model(
     "Place",
