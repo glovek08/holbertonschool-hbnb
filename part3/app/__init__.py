a6aaa3eb6d932164be0fe9e6f13574dfd887e833
glovek08 <gbrielbbarn@gmail.com>
2025-07-17 18:02:41 -0300

    feat: connection to MySQL database using PyMySQL

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -75,5 +75,6 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(auth_ns, path="/api/v1/auth")
 
     with app.app_context():
+        db.create_all()
         seed_admin()
     return app
--- part3/config.py
+++ part3/config.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import quote_plus
 
 
 class Config:
@@ -8,7 +9,13 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{}:{}@{}:{}/{}".format(
+        os.getenv("DB_USER", "pizza"),
+        quote_plus(os.getenv("DB_PASSWORD", "Pizza123@@")),
+        os.getenv("DB_HOST", "localhost"),
+        os.getenv("DB_PORT", "3306"),
+        os.getenv("DB_NAME", "hbnb_v1"),
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
