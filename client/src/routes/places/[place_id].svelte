c2bc6f3840cee65f4b493ffc362085e7f004056f
glovek08 <gbrielbbarn@gmail.com>
2025-08-04 18:52:25 -0300

    feat: add places routing structure with detail and catalog pages

---
+++ client/src/routes/places/[place_id].svelte
@@ -0,0 +1,7 @@
+<script>
+  import AuthBox from "../../components/AuthBox.svelte";
+</script>
+
+<AuthBox>
+  <h3>Place Details</h3>
+</AuthBox>
---
+++ client/src/routes/places/places_catalog.svelte
@@ -0,0 +1,12 @@
+<script>
+  import PlacesCatalog from "../../components/PlacesCatalog.svelte";
+  import AuthBox from "../../components/AuthBox.svelte";
+</script>
+
+<AuthBox>
+  <PlacesCatalog/>
+</AuthBox>
+
+<style>
+
+</style>
