2c6dcd3798078a340000935f21b516e85b330ff7
glovek08 <gbrielbbarn@gmail.com>
2025-08-04 19:46:24 -0300

    fix: routify correct routing proxy

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -67,7 +67,7 @@ class ApiService {
   }
 
   async getPlace(placeId) {
-    return this.request(`/places/${placeId}/`);
+    return this.request(`/places/${placeId}`);
   }
 
   // ******** AMENITIES KINGDOM ********
--- client/src/routes/places/[place_id].svelte
+++ client/src/routes/places/[place_id].svelte
@@ -1,7 +1,33 @@
 <script>
   import AuthBox from "../../components/AuthBox.svelte";
+  import api from "../../lib/api";
+  import { params } from "@roxi/routify";
+  import { onMount } from "svelte";
+
+  let place_id;
+  let place;
+
+  $: place_id = $params.place_id; // <- must understand this!
+  console.log($params);
+
+  async function fetchPlace() {
+    try {
+      place = await api.getPlace(place_id);
+    } catch (error) {
+      console.error(`Error fetching place: ${error}`);
+    }
+  }
+  onMount(() => {
+    fetchPlace();
+  });
 </script>
 
 <AuthBox>
-  <h3>Place Details</h3>
+  <h1 class="">Place Details</h1>
 </AuthBox>
+
+<style>
+  h1 {
+    font-size: 3rem;
+  }
+</style>
