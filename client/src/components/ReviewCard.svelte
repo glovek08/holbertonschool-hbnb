038114fb5427934b93e05e49fc8542f6bad3c7de
glovek08 <gbrielbbarn@gmail.com>
2025-08-12 18:31:40 -0300

    refactor: deconstruct review object inside place card component

--- client/src/components/ReviewCard.svelte
+++ client/src/components/ReviewCard.svelte
@@ -1,40 +1,68 @@
 <script>
   import RatingBox from "./RatingBox.svelte";
-  export let authorFirstName = "n/a";
-  export let authorLastName = "n/a";
-  export let rating = 0;
-  export let comment = "n/a";
-  // export let reviewAuthorID = ""
+
+  export let review = {};
+
+  $: ({
+    author_first_name: authorFirstName = "n/a",
+    author_last_name: authorLastName = "n/a", 
+    rating = 0,
+    comment = "n/a",
+    user_id: reviewAuthorID = ""
+  } = review);
 </script>
 
 <div class="review-container">
-  <RatingBox {rating} />
-  <h3 class="user-name-title">
-    <a href="/This will take to review author" aria-label="Review Author"
-      >{authorFirstName} {authorLastName}</a
-    >
-  </h3>
-  <p class="review-comment">{comment}</p>
+  <div class="review-header">
+    <h3 class="user-name-title">
+      <a href="/user/{reviewAuthorID}" aria-label="Review Author Profile">
+        {authorFirstName} {authorLastName}
+      </a>
+    </h3>
+    <RatingBox {rating} />
+  </div>
+  <div class="review-body">
+    <p class="review-comment">{comment}</p>
+  </div>
 </div>
 
 <style>
-  :root {
-    --review-background: rgba(48, 48, 51, 0.712);
-    --review-shadow: rgba(0, 0, 0, 0.5);
-  }
-  :root.light {
-    --review-background: white;
-    --review-shadow: rgba(0, 0, 0, 0.24);
-  }
   :global(.rating-container) {
     width: fit-content;
   }
   .review-container {
-    /* outline: 1px solid red; */
     border-radius: 20px;
-    background: var(--review-background);
-    width: clamp(300px, 100%, 800px);
+    background: var(--user-dashboard-card-bg, #fff);
+    box-shadow: 5px 5px 9px var(--user-dashboard-card-shadow, rgba(0,0,0,0.1));
+    width: clamp(300px, 90%, 800px);
     padding: 25px 30px 10px 30px;
-    box-shadow: 5px 5px 9px var(--review-shadow);
+    margin-bottom: 1rem;
+  }
+  .review-header {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .user-name-title {
+    margin-top: 5px;
+  }
+  .user-name-title a {
+    text-decoration: none;
+    color: inherit;
+  }
+  .user-name-title a:hover {
+    text-decoration: underline;
+  }
+  @media screen and (max-width: 800px) {
+    .review-container {
+      font-size: 1.5dvw;
+      padding: 1dvw 3dvw;
+    }
+  }
+  @media screen and (max-width: 400px) {
+    .review-container {
+      width: clamp(50px, 70%, 800px);
+    }
   }
 </style>
--- client/src/components/UserReviews.svelte
+++ client/src/components/UserReviews.svelte
@@ -4,30 +4,14 @@
   export let userReviews = [];
 </script>
 
+<h2>My Reviews</h2>
 {#if userReviews.length > 0}
-  <ul class="user-review-ul">
-    {#each userReviews as review}
-      <li>
-        <ReviewCard
-          authorFirstName={review.author_first_name}
-          authorLastName={review.author_last_name}
-          rating={review.rating}
-          comment={review.comment}
-        />
-      </li>
-    {/each}
-  </ul>
+  {#each userReviews as review}
+    <ReviewCard review={review}/>
+  {/each}
 {:else}
   <p>No reviews found.</p>
 {/if}
 
 <style>
-  .user-review-ul {
-    width: 100%;
-    padding: 20px;
-  }
-  .user-review-ul li {
-    padding: 0;
-    margin: 0;
-  }
 </style>
